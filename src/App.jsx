6767feeae1a7564a6b7c5912060ff75ea4bbd0ee
SarthakPaandey <145528240+SarthakPaandey@users.noreply.github.com>
2024-06-15 18:28:34 +0000

    chore: update package.json homepage URL and siteProps in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Sarthak Pandey",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "sarthakpandey973@gamil.com",
+  gitHub: "https://github.com/SarthakPaandey",
+  linkedIn: "https://www.linkedin.com/in/sarthak-pandey973/",
 };
 
 const primaryColor = "#4E567E";
